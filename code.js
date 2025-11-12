6fd050f53e52d48be03b6bf66642d1199f85dcf6
Godson M.D <121962305+Godsonmd@users.noreply.github.com>
2025-11-12 07:53:57 +0530

    Update code.js

--- code.js
+++ code.js
@@ -3,11 +3,7 @@ function show () {
     var name1,name2;
 name1=document.iform.player1.value;
 name2=document.iform.player2.value;
-if(name1==="" || name2==="")
-   {document.getElementById("demo").innerText="";return;}
-else
-{
-    if(name1.charAt()===" " || name2.charAt()===" ")
+if(!name1 && !name2)
         {document.getElementById("demo").innerText="";return;}
     else
     {
@@ -16,12 +12,17 @@ else
         localStorage.setItem("NAMEB",name2);
     }
 }
-}
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    go();
+});
 function go(){
 name1=document.iform.player1.value;
 name2=document.iform.player2.value;
 localStorage.setItem("NAMEA",name1);
 localStorage.setItem("NAMEB",name2);
+window.location.href="play.html";
 }
 function sound(){
     document.getElementById("bu").play();
